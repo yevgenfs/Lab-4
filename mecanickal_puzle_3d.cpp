959e7a6a88acac2696acabb0f5403e2d27e6b374
Yevhen Surcov <yevgenfs2@gmail.com>
2018-11-20 23:14:03 +0200

    realization of class mecanickal_puzle_3d with 16 metods

---
+++ mecanickal_puzle_3d.cpp
@@ -0,0 +1,106 @@
+
+#include "mecanickal_puzle_3d.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+
+void mecanickal_puzle_3d::Inputhow_many_element()
+{
+	cout << "how many elements " << endl;
+	cin >> mecanickal_puzle_3d::how_many_element;
+
+}
+
+void mecanickal_puzle_3d::outputhow_many_element()
+{
+	cout << "how many elements " << mecanickal_puzle_3d::how_many_element << endl;
+
+}
+
+void mecanickal_puzle_3d::Inputguarantee_in_month()
+{
+	cout << "guarantee_in_month " << endl;
+	cin >> mecanickal_puzle_3d::guarantee_in_month;
+
+}
+
+void mecanickal_puzle_3d::outputguarantee_in_month()
+{
+	cout << "guarantee_in_month " << mecanickal_puzle_3d::guarantee_in_month << endl;
+}
+
+
+void mecanickal_puzle_3d::Inputmaterial()
+{
+	cout << "material " << endl;
+	cin >> mecanickal_puzle_3d::material;
+
+}
+
+void mecanickal_puzle_3d::outputmaterial()
+{
+	cout << " material " << mecanickal_puzle_3d::material << endl;
+}
+
+void mecanickal_puzle_3d::Inputtype()
+{
+	cout << "type " << endl;
+	cin >> mecanickal_puzle_3d::type;
+
+}
+
+void mecanickal_puzle_3d::outputtype()
+{
+	cout << " type " << mecanickal_puzle_3d::type << endl;
+}
+
+void mecanickal_puzle_3d::Inputname()
+{
+	cout << "name " << endl;
+	cin >> mecanickal_puzle_3d::name;
+
+}
+
+void mecanickal_puzle_3d::outputname()
+{
+	cout << " name " << mecanickal_puzle_3d::name << endl;
+}
+
+void mecanickal_puzle_3d::Inputday()
+{
+	cout << "day " << endl;
+	cin >> mecanickal_puzle_3d::day;
+	;
+}
+
+void mecanickal_puzle_3d::outputday()
+{
+	cout << "day " << mecanickal_puzle_3d::day << endl;
+}
+
+void mecanickal_puzle_3d::Inputyear()
+{
+	cout << "year " << endl;
+	cin >> mecanickal_puzle_3d::year;
+
+}
+
+void mecanickal_puzle_3d::outputyear()
+{
+	cout << "year " << mecanickal_puzle_3d::year << endl;
+}
+
+void mecanickal_puzle_3d::Inputowner()
+{
+	cout << "owner " << endl;
+	cin >> mecanickal_puzle_3d::owner;
+
+}
+
+void mecanickal_puzle_3d::outputowner()
+{
+	cout << " owner " << mecanickal_puzle_3d::owner << endl;
+}
