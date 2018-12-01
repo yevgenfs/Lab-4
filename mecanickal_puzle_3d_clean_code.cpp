0f5e3541ea0b97f7aba015f71d58febc7ce75ba0
Yevhen Surcov <yevgenfs2@gmail.com>
2018-12-01 10:52:33 +0200

    ralisation of  lab-4 on clean code

---
+++ mecanickal_puzle_3d_clean_code.cpp
@@ -0,0 +1,107 @@
+#include "pch.h"
+#include "mecanickal_puzle_3d.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+
+void mecanickal_puzle_3d::Inputhow_many_element()
+{
+	cout << "how many elements " << endl;
+	cin >> mecanickal_puzle_3d::how_many_element;
+
+}
+
+void mecanickal_puzle_3d::outputhow_many_element()
+{
+	cout << "how many elements " << mecanickal_puzle_3d::how_many_element << endl;
+
+}
+
+void mecanickal_puzle_3d::Inputguarantee_in_month()
+{
+	cout << "guarantee_in_month " << endl;
+	cin >> mecanickal_puzle_3d::guarantee_in_month;
+
+}
+
+void mecanickal_puzle_3d::outputguarantee_in_month()
+{
+	cout << "guarantee_in_month " << mecanickal_puzle_3d::guarantee_in_month << endl;
+}
+
+
+void mecanickal_puzle_3d::Inputmaterial()
+{
+	cout << "material " << endl;
+	cin >> mecanickal_puzle_3d::material_puzle;
+
+}
+
+void mecanickal_puzle_3d::outputmaterial()
+{
+	cout << " material " << mecanickal_puzle_3d::material_puzle << endl;
+}
+
+void mecanickal_puzle_3d::Inputtype()
+{
+	cout << "type_puzle " << endl;
+	cin >> mecanickal_puzle_3d::type_puzle;
+	;
+
+}
+
+void mecanickal_puzle_3d::outputtype()
+{
+	cout << " type_puzle " << mecanickal_puzle_3d::type_puzle << endl;
+}
+
+void mecanickal_puzle_3d::Inputname()
+{
+	cout << "name_puzle " << endl;
+	cin >> mecanickal_puzle_3d::name_puzle;
+
+}
+
+void mecanickal_puzle_3d::outputname()
+{
+	cout << " name_puzle " << mecanickal_puzle_3d::name_puzle << endl;
+}
+
+void mecanickal_puzle_3d::Inputday()
+{
+	cout << "day_of_purchase " << endl;
+	cin >> mecanickal_puzle_3d::day_of_purchase;
+	;
+}
+
+void mecanickal_puzle_3d::outputday()
+{
+	cout << "day_of_purchase " << mecanickal_puzle_3d::day_of_purchase << endl;
+}
+
+void mecanickal_puzle_3d::Inputyear()
+{
+	cout << "year_of_purchase " << endl;
+	cin >> mecanickal_puzle_3d::year_of_purchase;
+
+}
+
+void mecanickal_puzle_3d::outputyear()
+{
+	cout << "year_of_purchase " << mecanickal_puzle_3d::year_of_purchase << endl;
+}
+
+void mecanickal_puzle_3d::Inputowner()
+{
+	cout << "owners_name " << endl;
+	cin >> mecanickal_puzle_3d::owners_name;
+
+}
+
+void mecanickal_puzle_3d::outputowner()
+{
+	cout << " owners_name " << mecanickal_puzle_3d::owners_name << endl;
+}
